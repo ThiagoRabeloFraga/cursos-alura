bc9929d07e9012d0ae9abca07f437d3afa1c8cea
Thiago Rabelo Fraga <thiagoo.fraga@gmail.com>
2022-10-31 23:06:35 -0300

    Encapsulamento

--- cursos/005/conta.py
+++ cursos/005/conta.py
@@ -2,24 +2,28 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite=1000.0) -> None:
         # print("Construindo objeto... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
 
 
 if __name__ == '__main__':
     conta = Conta(123, "Nico", 55.5, 1000.0)
-    conta1 = Conta(1, "Fulano", 0.0)
+    conta1 = Conta(1, "Fulano", 100.0, 1000.0)
     conta2 = Conta(2, "Beltrano", 0.0)
     conta3 = Conta(3, "Sicrano", 0.0, 2000.0)
 
@@ -28,3 +32,6 @@ if __name__ == '__main__':
     conta.extrato()
     conta.saca(10.0)
     conta.extrato()
+
+    conta2.transfere(10.0, conta)
+    conta2.extrato()
